559b5c5adcda39e0b679c386b877c2594c4fe730
GH <982369788@qq.com>
2019-09-20 17:13:46 +0800

    build: 起步

--- lesson01/webpack.config.js
+++ lesson01/webpack.config.js
@@ -3,7 +3,6 @@ const path = require('path');
 module.exports = {
     entry: './src/index.js',
     output: {
-        filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist')
     }
 };
