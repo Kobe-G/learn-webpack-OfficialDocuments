e1390b75c5b63e31e8bc2fcc727848a01d8903a2
GH <982369788@qq.com>
2019-09-21 12:45:00 +0800

    add 07

---
+++ lesson07/webpack.config.js
@@ -0,0 +1,21 @@
+const path = require('path');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+
+module.exports = {
+    entry: {
+        index: './src/index.js',
+        print: './src/math.js',
+    },
+    mode: "development",
+    output: {
+        filename: '[name].[hash:4].js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    plugins: [
+        new HtmlWebpackPlugin(),
+        new CleanWebpackPlugin(),
+    ]
+
+};
